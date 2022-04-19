c333f4e72b093acc41cfc70e3514117e4a6aac97
JuandeCuesta <juande.cuesta@gmail.com>
2022-04-19 19:03:51 +0200

    Navegacion login y cerrar sesion

--- app/src/main/java/edu/juandecuesta/t_fitprogress/MainActivity.kt
+++ app/src/main/java/edu/juandecuesta/t_fitprogress/MainActivity.kt
@@ -1,7 +1,9 @@
 package edu.juandecuesta.t_fitprogress
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import edu.juandecuesta.t_fitprogress.databinding.ActivityMainBinding
+import edu.juandecuesta.t_fitprogress.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +34,6 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
@@ -53,8 +55,30 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.nav_logout ->{
+                val myIntent = Intent (this, LoginActivity::class.java)
+                startActivity(myIntent)
+                finish()
+                return true
+            }else -> super.onOptionsItemSelected(item)
+        }
+
+
+
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isOpen){
+            binding.drawerLayout.close()
+        }else super.onBackPressed()
+    }
+
+
 }
---
+++ app/src/main/java/edu/juandecuesta/t_fitprogress/ui/login/LoginActivity.kt
@@ -0,0 +1,61 @@
+package edu.juandecuesta.t_fitprogress.ui.login
+
+import android.app.Activity
+import android.content.Intent
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
+import edu.juandecuesta.t_fitprogress.MainActivity
+import edu.juandecuesta.t_fitprogress.databinding.ActivityLoginBinding
+
+import edu.juandecuesta.t_fitprogress.R
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val password = binding.password
+        val login = binding.login
+        val loading = binding.loading
+
+        login.setOnClickListener {
+            loading.visibility = View.VISIBLE
+            val myIntent = Intent (this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }
+    }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+    }
+}
+
+/**
+ * Extension function to simplify setting an afterTextChanged action to EditText components.
+ */
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
+}
