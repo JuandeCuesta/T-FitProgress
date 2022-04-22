0ff839070b8b569a9772d9e7459a33e0859cd7a5
JuandeCuesta <juande.cuesta@gmail.com>
2022-04-22 20:07:19 +0200

    Autentication y registro firebase deportistas

---
+++ app/src/main/java/edu/juandecuesta/t_fitprogress/documentFirebase/entrenadorDB.kt
@@ -0,0 +1,10 @@
+package edu.juandecuesta.t_fitprogress.documentFirebase
+
+class entrenadorDB {
+    var apellido:String = ""
+    var nombre:String = ""
+    var soyEntrenador:Boolean = true
+    var deportistas:MutableList<String> = arrayListOf()
+    var ejercicios:MutableList<String> = arrayListOf()
+    var entrenamientos:MutableList<String> = arrayListOf()
+}
